9be2a3d104cbca4434c59b5005ad51a56c4e9d74
Kranthikumarmunjala <Kranthiq3@gmail.com>
2026-04-16 13:26:35 +0530

    third commit

--- tasks/models.py
+++ tasks/models.py
@@ -1,15 +1,11 @@
 from django.db import models
 
 class Task(models.Model):
-    PRIORITY_CHOICES = [
-        ('Low', 'Low'),
-        ('Medium', 'Medium'),
-        ('High', 'High'),
-    ]
-
+    PRIORITY_CHOICES = [('Low', 'Low'), ('Medium', 'Medium'), ('High', 'High')]
+    
     title = models.CharField(max_length=255)
     priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES)
-    estimated_time = models.IntegerField()  # In minutes
+    estimated_time = models.IntegerField()
     created_at = models.DateTimeField(auto_now_add=True)
     is_completed = models.BooleanField(default=False)
     is_locked = models.BooleanField(default=False)
--- tasks/urls.py
+++ tasks/urls.py
@@ -1,13 +1,3 @@
-# from django.urls import path, include
-# from rest_framework.routers import DefaultRouter
-# from .views import TaskViewSet
-
-# router = DefaultRouter()
-# router.register(r'tasks', TaskViewSet)
-
-# urlpatterns = [
-#     path('', include(router.urls)),
-# ]
 from django.urls import path
 from . import views
 
